983a63044feea0d6c0711664971d6a202bc1ae79
buvoli <buvoli@uw.edu>
2020-01-03 18:00:35 -0800

    removed console.log

--- src/commands/shell.ts
+++ src/commands/shell.ts
@@ -21,12 +21,8 @@ export default class Shell extends StackCommand {
     const runner  = this.newRunner(flags.explicit)
     const stack_path = this.fullStackPath(flags.stack)
 
-    console.log(this.project_settings.configFiles)
-
     var result = IfBuiltAndLoaded(builder, flags, stack_path, this.project_settings.configFiles,
       (configuration, containerRoot, hostRoot) => {
-
-        console.log(configuration)
 
         if(hostRoot)
         {
