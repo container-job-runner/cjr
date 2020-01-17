564fd7910b5ed837d50d445a7183603681f94063
Tommaso Buvoli <buvoli@Tommasos-MacBook-Pro.local>
2020-01-16 17:09:17 -0700

    added hostRoot flag to build command

--- src/commands/stack/build.ts
+++ src/commands/stack/build.ts
@@ -6,6 +6,7 @@ export default class Build extends StackCommand {
   static args = []
   static flags = {
     stack:    flags.string({env: 'STACK', default: false}),
+    hostRoot: flags.string({env: 'HOSTROOT', default: false}),
     explicit: flags.boolean({default: false}),
     silent:   flags.boolean({default: false}),
     nocache:  flags.boolean({default: false})
