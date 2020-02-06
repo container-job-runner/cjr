87cace41fc02b4db93cd063daf2c9936641c5cdf
buvoli <buvoli@uw.edu>
2020-02-06 02:07:59 -0800

    file-tools now uses shell-command

--- src/lib/fileio/file-tools.ts
+++ src/lib/fileio/file-tools.ts
@@ -2,36 +2,37 @@ import * as fs from 'fs-extra'
 import * as os from 'os'
 import * as path from 'path'
 import {ValidatedOutput} from '../validated-output'
-import {ShellCMD} from '../shellcmd'
+import {ShellCommand} from '../shell-command'
 import {JSTools} from '../js-tools'
 
 export class FileTools
 {
 
-  static existsDir(path_str: string)
+  static existsDir(path_str: string) // determines if directory exists
   {
     return fs.existsSync(path_str) && fs.lstatSync(path_str).isDirectory()
   }
 
-  static existsFile(path_str: string)
+  static existsFile(path_str: string) // determines if file exists
   {
     return fs.existsSync(path_str) && fs.lstatSync(path_str).isFile()
   }
 
-  static mktempDir(parent_abs_path: string, shell:ShellCMD = new ShellCMD(false, false))
+  // creates a temporary directory inside parent_abs_path
+  static mktempDir(parent_abs_path: string, shell:ShellCommand = new ShellCommand(false, false))
   {
     fs.ensureDir(parent_abs_path)
     switch(os.platform())
     {
       case "darwin":
-        return shell.output('mktemp', {d: {shorthand: true}}, [path.join(parent_abs_path, "tmp.XXXXXXXXXX")], {}, "trim")
+        return shell.output('mktemp', {d: {}}, [path.join(parent_abs_path, "tmp.XXXXXXXXXX")], {}, "trim")
       case "linux":
         const flags = {
-          tmpdir: {shorthand: false, value: parent_abs_path},
-          directory: {shorthand: false}
+          tmpdir: parent_abs_path,
+          directory: {}
         }
         return shell.output('mktemp', flags, [], {}, "trim")
-      default: // unsafe
+      default: // not thread safe
         var data = fs.ensureDir(path.join(parent_abs_path, `tmp.${JSTools.randomString(10)}`))
         return new ValidatedOutput(true, data)
     }
