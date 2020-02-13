e4454138132d0c59177056d2c761316ca5f7a9c0
buvoli <buvoli@uw.edu>
2020-02-12 21:49:10 -0800

    added custom isBuilt Function for podman to address differences between podman images and docker images

--- src/lib/drivers/podman/podman-build-driver.ts
+++ src/lib/drivers/podman/podman-build-driver.ts
@@ -10,6 +10,22 @@ export class PodmanBuildDriver extends DockerBuildDriver
     protected json_output_format = "json"
     protected configuration_constructor = PodmanConfiguration // pointer to configuration class constructor
 
+    isBuilt(stack_path: string)
+    {
+      const command = `${this.base_command} ${this.sub_commands["images"]}`;
+      const args:Array<string> = []
+      const flags:Dictionary = {
+        filter: `reference=${this.imageName(stack_path)}`
+      }
+      this.addJSONFormatFlag(flags);
+      var result = this.shell.output(command, flags, args, {}, this.json_output_format)
+      if(!result.success) return false
+      // extra logic since podman images --reference=name:tag is equivalent to docker images --reference=*name:tag
+      return result.data.some((image_data:ValidatedOutput) =>
+        image_data.names.some((name: string) =>
+          (new RegExp(`/${this.imageName(stack_path)}$`)).test(name)))
+    }
+
     protected addJSONFormatFlag(flags: Dictionary)
     {
       flags["format"] = {shorthand: false, value: 'json'}
