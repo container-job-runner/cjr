fcede67ea85abaaf2dc5e4824a444ae2a6450519
buvoli <buvoli@uw.edu>
2020-03-07 03:46:39 -0800

    added job:state command to allow for job monitoring from a shell script

---
+++ src/commands/job/state.ts
@@ -0,0 +1,25 @@
+import {flags} from '@oclif/command'
+import {StackCommand} from '../../lib/commands/stack-command'
+import {matchingJobInfo} from '../../lib/functions/run-functions'
+import {printResultState} from '../../lib/functions/misc-functions'
+
+export default class State extends StackCommand {
+  static description = 'get the current state of a single job'
+  static args = [{name: 'id', required: true}]
+  static flags = {
+    stack: flags.string({env: 'STACK'})
+  }
+  static strict = true;
+
+  async run()
+  {
+    const {args, flags} = this.parse(State)
+    const runner  = this.newRunner(flags.explicit)
+    const stack_path = (flags?.stack) ? this.fullStackPath(flags.stack) : ""
+    const result = matchingJobInfo(runner, [args.id], stack_path)
+    if(!result.success) return printResultState(result)
+    const job_info = result.data
+    console.log(job_info[0].status)
+  }
+
+}
