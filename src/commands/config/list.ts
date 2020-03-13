b6d526fe144c5cebfaa65b90986b82d917a3ec51
buvoli <buvoli@uw.edu>
2020-03-13 04:04:33 -0700

    added json output for config:list

--- src/commands/config/list.ts
+++ src/commands/config/list.ts
@@ -5,16 +5,27 @@ import * as chalk from 'chalk'
 
 export default class List extends StackCommand {
   static description = 'List all CLI parameters and data directories.'
+  static flags = {
+    json: flags.boolean({default: false}),
+  }
   static strict = true;
 
-  async run() {
-    this.log(chalk`\n-- {bold CLI Settings} -----------------------------\n`)
-    const raw_data = this.settings.getRawData()
-    for(var key in raw_data) {
-      console.log(chalk`   {italic ${key}}: {green ${raw_data[key]}}`)
-    }
-    this.log(chalk`\n-- {bold CLI Data Path:} contains temporary data ----\n\n   ${this.config.dataDir}`)
-    this.log(chalk`\n-- {bold CLI Config Path:} contains settings files -----\n\n   ${this.config.configDir}`, '\n')
 
+  async run() {
+    const {flags} = this.parse(List)
+    const raw_data = this.settings.getRawData()
+    if(flags.json) // -- json output -------------------------------------------
+    {
+      console.log(JSON.stringify(raw_data))
+    }
+    else // -- standard output -------------------------------------------------
+    {
+      this.log(chalk`\n-- {bold CLI Settings} -----------------------------\n`)
+      for(var key in raw_data) {
+        console.log(chalk`   {italic ${key}}: {green ${raw_data[key]}}`)
+      }
+      this.log(chalk`\n-- {bold CLI Data Path:} contains temporary data ----\n\n   ${this.config.dataDir}`)
+      this.log(chalk`\n-- {bold CLI Config Path:} contains settings files -----\n\n   ${this.config.configDir}`, '\n')
+    }
   }
 }
