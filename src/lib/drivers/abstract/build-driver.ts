01d2e2216cf1ad98d9acdacf583a548cdfe03596
buvoli <buvoli@uw.edu>
2020-03-22 00:21:30 -0700

    Updated abstract class BuildDriver: (1) functions build and isBuilt() require a Configuration instead of Array<string> overloaded_config_paths, (2) removeImage now accepts an optional configuration, and (3) validate only validates stack and its config file, not external configuration files.

--- src/lib/drivers/abstract/build-driver.ts
+++ src/lib/drivers/abstract/build-driver.ts
@@ -7,11 +7,11 @@ type Dictionary = {[key: string]: any}
 
 export abstract class BuildDriver extends ContainerDriver
 {
-  abstract validate(stack_path: string, overloaded_config_paths: Array<string>): ValidatedOutput;
-  abstract build(stack_path: string, overloaded_config_paths: Array<string>, nocache?:boolean): ValidatedOutput;
-  abstract isBuilt(stack_path: string): boolean;
+  abstract validate(stack_path: string): ValidatedOutput;
+  abstract build(stack_path: string, configuration: StackConfiguration, nocache?:boolean): ValidatedOutput;
+  abstract isBuilt(stack_path: string, configuration: StackConfiguration): boolean;
   abstract loadConfiguration(stack_path: string, overloaded_config_paths: Array<string>): ValidatedOutput;
-  abstract removeImage(stack_path: string): ValidatedOutput;
+  abstract removeImage(stack_path: string, configuration?: StackConfiguration): ValidatedOutput;
   abstract copy(stack_path: string, copy_path: string, configuration?: StackConfiguration): ValidatedOutput;       // copies stack files and configuration to new folder
   abstract copyConfig(stack_path: string, copy_path: string, configuration?: StackConfiguration): ValidatedOutput; // copies stack configuration to new folder
 }
