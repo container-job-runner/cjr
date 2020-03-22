921b3411608273468a17f78d08a915289f5c42d0
buvoli <buvoli@uw.edu>
2020-03-22 00:43:49 -0700

    added stacks-dir parameter, and removed unecessary call to imageName()

--- src/commands/jupyter/list.ts
+++ src/commands/jupyter/list.ts
@@ -10,6 +10,7 @@ export default class List extends StackCommand {
   static args = []
   static flags = {
     stack: flags.string({env: 'STACK'}),
+    "stacks-dir": flags.string({default: "", description: "override default stack directory"}),
     explicit: flags.boolean({default: false})
   }
   static strict = false;
@@ -17,12 +18,10 @@ export default class List extends StackCommand {
   async run()
   {
     const {argv, flags} = this.parse(List)
-    this.augmentFlagsWithProjectSettings(flags, {stack:true})
+    this.augmentFlagsWithProjectSettings(flags, {stack:true, "stacks-dir": false})
     const runner  = this.newRunner(flags.explicit)
-    const stack_path = this.fullStackPath(flags.stack as string)
-    const image_name = runner.imageName(stack_path)
+    const stack_path = this.fullStackPath((flags.stack as string), flags["stacks-dir"] || "")
     const jupyter_id = jobNameLabeltoID(runner, JUPYTER_JOB_NAME, stack_path, "running");
-
 
     if(jupyter_id === false)
     {
