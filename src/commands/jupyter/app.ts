63d2791a9e1d40219225a3ad5e97293df487de64
buvoli <buvoli@uw.edu>
2020-03-22 00:43:17 -0700

    renamed class from Stop to App, added stacks-dir parameter, and removed unecessary call to imageName()

--- src/commands/jupyter/app.ts
+++ src/commands/jupyter/app.ts
@@ -6,22 +6,22 @@ import {ValidatedOutput} from '../../lib/validated-output'
 import {jobNameLabeltoID} from '../../lib/functions/run-functions'
 import {JUPYTER_JOB_NAME} from '../../lib/constants'
 
-export default class Stop extends StackCommand {
+export default class App extends StackCommand {
   static description = 'start the Jupyter app.'
   static args = []
   static flags = {
     stack: flags.string({env: 'STACK'}),
+    "stacks-dir": flags.string({default: "", description: "override default stack directory"}),
     explicit: flags.boolean({default: false})
   }
   static strict = false;
 
   async run()
   {
-    const {argv, flags} = this.parse(Stop)
-    this.augmentFlagsWithProjectSettings(flags, {stack:true})
+    const {argv, flags} = this.parse(App)
+    this.augmentFlagsWithProjectSettings(flags, {stack:true, "stacks-dir": false})
     const runner  = this.newRunner(flags.explicit)
-    const stack_path = this.fullStackPath(flags.stack as string)
-    const image_name = runner.imageName(stack_path)
+    const stack_path = this.fullStackPath((flags.stack as string), flags["stacks-dir"] || "")
     const jupyter_id = jobNameLabeltoID(runner, JUPYTER_JOB_NAME, stack_path, "running");
     var result: ValidatedOutput;
 
