b7cc92d9dd1d4816a75963067a7415759b671281
buvoli <buvoli@uw.edu>
2020-03-22 00:41:07 -0700

    updated stack:build and stack:rmi to adhere to use functions from build-function

--- src/commands/stack/build.ts
+++ src/commands/stack/build.ts
@@ -1,7 +1,7 @@
 import {flags} from '@oclif/command'
 import {StackCommand} from '../../lib/commands/stack-command'
 import {JSTools} from '../../lib/js-tools'
-import {ErrorStrings} from '../../lib/error-strings'
+import {buildAndLoad} from '../../lib/functions/build-functions'
 import {printResultState} from '../../lib/functions/misc-functions'
 
 export default class Build extends StackCommand {
@@ -24,8 +24,14 @@ export default class Build extends StackCommand {
     const stack_list = (argv.length > 0) ? argv : (JSTools.arrayWrap(flags.stack) || []) // add arrayWrap since parseWithLoad will return scalar
     const builder = this.newBuilder(flags.explicit, flags.silent)
     stack_list.map((stack_name:string) => {
-      const stack_path = this.fullStackPath(stack_name, flags["stacks-dir"])
-      printResultState(builder.build(stack_path, flags['config-files'], flags['no-cache']))
+      printResultState(
+        buildAndLoad(
+          builder,
+          (flags["no-cache"]) ? "build-nocache" : "build",
+          this.fullStackPath(stack_name, flags["stacks-dir"]),
+          flags['config-files']
+        )
+      )
     });
   }
 
--- src/commands/stack/rmi.ts
+++ src/commands/stack/rmi.ts
@@ -3,29 +3,30 @@ import {StackCommand} from '../../lib/commands/stack-command'
 import {JSTools} from '../../lib/js-tools'
 import {ErrorStrings} from '../../lib/error-strings'
 import {printResultState} from '../../lib/functions/misc-functions'
+import {removeImage} from '../../lib/functions/build-functions'
 
 export default class RMI extends StackCommand {
   static description = 'Delete an image for any number of stacks.'
   static args = [{name:'stack'}]
   static flags = {
     stack: flags.string({env: 'STACK', multiple: true}),
-    hostRoot: flags.string({env: 'HOSTROOT'}),
+    "config-files": flags.string({default: [], multiple: true, description: "additional configuration file to override stack configuration"}),
     explicit: flags.boolean({default: false}),
     silent:   flags.boolean({default: false}),
-    "stacks-dir": flags.string({default: "", description: "override default stack directory"})
+    "stacks-dir": flags.string({default: "", description: "override default stack directory"}),
+    "all-configurations": flags.boolean({default: false})
   }
   static strict = false;
 
   async run()
   {
     const {argv, flags} = this.parse(RMI)
-    this.augmentFlagsWithProjectSettings(flags, {stack:false, "stacks-dir": false})
+    this.augmentFlagsWithProjectSettings(flags, {stack:false, "stacks-dir": false, "config-files": false})
     const stack_list = (argv.length > 0) ? argv : (JSTools.arrayWrap(flags.stack) || []) // add arrayWrap since parseWithLoad will return scalar
     const builder = this.newBuilder(flags.explicit, flags.silent)
     stack_list.map((stack_name:string) => {
       const stack_path = this.fullStackPath(stack_name, flags["stacks-dir"])
-      if(!flags.silent) console.log(ErrorStrings.STACK.NO_STACK_SPECIFIED(stack_name, stack_path))
-      printResultState(builder.removeImage(stack_path))
+      removeImage(builder, stack_path, flags['all-configurations'], flags['config-files'])
     });
   }
 
