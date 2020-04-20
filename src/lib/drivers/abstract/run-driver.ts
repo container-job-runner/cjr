681494f3f53e092a4c62e073a5637cd921c40e28
buvoli <buvoli@uw.edu>
2020-04-20 12:26:59 +0000

    added new types for jobInfo

--- src/lib/drivers/abstract/run-driver.ts
+++ src/lib/drivers/abstract/run-driver.ts
@@ -1,16 +1,44 @@
 // ===========================================================================
-// Settings: A class for getting and setting
+// RunDriver: Abstract class for running jobs and accessing their info
 // ===========================================================================
 
-import {ContainerDriver} from "./container-driver"
-import {ValidatedOutput} from "../../validated-output"
-import {StackConfiguration} from "../../config/stacks/abstract/stack-configuration"
+import { ContainerDriver } from "./container-driver"
+import { ValidatedOutput } from "../../validated-output"
+import { StackConfiguration } from "../../config/stacks/abstract/stack-configuration"
 
 // -- types --------------------------------------------------------------------
 export type Dictionary = {[key: string]: any}
+export type JobPortInfo = {
+  ip: string,
+  containerPort: number,
+  hostPort: number
+}
+export type JobState = "created"|"restarting"|"running"|"exited"|"paused"|"dead"|"unknown"
+export type JobInfo = {
+  id:      string,
+  names:   Array<string>,
+  command: string,
+  status:  string,
+  state:   JobState,
+  stack:   string
+  labels:  {[key: string]: string},
+  ports:   Array<JobPortInfo>
+}
 
 export abstract class RunDriver extends ContainerDriver
 {
+  // ---------------------------------------------------------------------------
+  // JOBINFO - returns information on running and completed jobs
+  // -- Parameters -------------------------------------------------------------
+  // 1. stack_paths: Array<string> - jobInfo only returns jobs whose stack_path
+  //    matches with one of the values in this array. If stack_paths then jobs
+  //    will not be filtered by stack_path.
+  // 2. job_states: Array<string> - jobInfo only returns jobs whose state matches
+  //    with one of the values in this array. if job_states=[] then jobs will
+  //    not be filtered by job_state.
+  // -- Returns ----------------------------------------------------------------
+  // Array<JobInfo> - information from job
+  // ---------------------------------------------------------------------------
   abstract jobInfo(stack_paths: Array<string>, job_states: Array<string>) : Array<Dictionary>;
   abstract jobStart(stack_path: string, configuration: StackConfiguration, callbacks:Dictionary): ValidatedOutput;
   abstract jobLog(id: string) : ValidatedOutput;
