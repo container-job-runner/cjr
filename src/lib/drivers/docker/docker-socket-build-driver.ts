3956e2ca12c51cb2d694d27cb9fca7ef03487e29
buvoli <buvoli@uw.edu>
2020-05-03 12:52:43 +0000

    updated blank template

--- src/lib/drivers/docker/docker-socket-build-driver.ts
+++ src/lib/drivers/docker/docker-socket-build-driver.ts
@@ -1,63 +1,39 @@
 
-import {ValidatedOutput} from "../../validated-output"
-import {StackConfiguration} from "../../config/stacks/abstract/stack-configuration"
-import {ShellCommand} from "../../shell-command"
-import {BuildDriver, Dictionary} from '../abstract/build-driver'
-import {DockerStackConfiguration} from '../../config/stacks/docker/docker-stack-configuration'
+import { ValidatedOutput } from "../../validated-output"
+import { StackConfiguration } from "../../config/stacks/abstract/stack-configuration"
+import { ShellCommand } from "../../shell-command"
+import { BuildDriver } from '../abstract/build-driver'
+import { DockerStackConfiguration } from '../../config/stacks/docker/docker-stack-configuration'
+import { Dictionary } from '../../constants'
 
 export class DockerSocketBuildDriver extends BuildDriver
 {
   protected socket:string
 
-  constructor(shell: ShellCommand, options: {tag: string, socket: string})
+  constructor(shell: ShellCommand, options: {socket: string})
   {
-    super(shell, options.tag)
+    super(shell)
     this.socket = options.socket
   }
 
-  validate(stack_path: string): ValidatedOutput<Dictionary>
-  {
-    return new ValidatedOutput(true, {})
-  }
-
-  build(stack_path: string, configuration: StackConfiguration, options?: Dictionary): ValidatedOutput<undefined>
+  build(configuration: DockerStackConfiguration, options?: Dictionary): ValidatedOutput<undefined>
   {
     return new ValidatedOutput(true, undefined)
   }
 
-  isBuilt(stack_path: string, configuration: StackConfiguration): boolean
+  isBuilt(configuration: StackConfiguration<any>): boolean
   {
-    return true
+    return false
   }
 
-  loadConfiguration(stack_path: string, overloaded_config_paths: Array<string>): ValidatedOutput<DockerStackConfiguration>
-  {
-    return new ValidatedOutput(true, this.emptyConfiguration())
-  }
-
-  removeImage(stack_path: string, configuration?: StackConfiguration): ValidatedOutput<undefined>
+  removeImage(configuration: DockerStackConfiguration): ValidatedOutput<undefined>
   {
     return new ValidatedOutput(true, undefined)
   }
 
-  copy(stack_path: string, copy_path: string, configuration?: StackConfiguration): ValidatedOutput<undefined>
+  removeAllImages(stack_path: string): ValidatedOutput<undefined>
   {
     return new ValidatedOutput(true, undefined)
-  }
-
-  copyConfig(stack_path: string, copy_path: string, configuration?: StackConfiguration): ValidatedOutput<undefined>
-  {
-    return new ValidatedOutput(true, undefined)
-  }
-
-  emptyConfiguration()
-  {
-    return new DockerStackConfiguration()
-  }
-
-  imageName(stack_path: string, prefix: string="") // Docker only accepts lowercase image names
-  {
-    return super.imageName(stack_path, prefix).toLowerCase()
   }
 
 }
