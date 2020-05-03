182b002cc2db34d1be08cb58cb84382f7abc2b29
buvoli <buvoli@uw.edu>
2020-05-03 12:56:06 +0000

    remove unecessary files since podman configuration is identical to docker

--- src/lib/config/stacks/podman/podman-stack-configuration.ts
+++
@@ -1,8 +0,0 @@
-
-import {DockerStackConfiguration} from '../docker/docker-stack-configuration'
-
-// Class for docker configuration
-export class PodmanStackConfiguration extends DockerStackConfiguration
-{
-  protected valid_flag_fieldnames = ["network", "security-opt", "userns", "mac-address", "net"]
-}
