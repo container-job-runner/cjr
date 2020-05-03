5c9649f6b884a19e0722ad924fe0996f21070efc
buvoli <buvoli@uw.edu>
2020-05-03 12:50:06 +0000

    updated blank templates for PodmanSocketDrivers

--- src/lib/drivers/podman/podman-socket-build-driver.ts
+++ src/lib/drivers/podman/podman-socket-build-driver.ts
@@ -1,12 +1,6 @@
-import {Dictionary} from '../abstract/build-driver'
-import {DockerSocketBuildDriver} from '../docker/docker-socket-build-driver'
-import {PodmanStackConfiguration} from '../../config/stacks/podman/podman-stack-configuration'
+import { DockerSocketBuildDriver } from '../docker/docker-socket-build-driver'
 
 export class PodmanSocketBuildDriver extends DockerSocketBuildDriver
 {
-    emptyConfiguration()
-    {
-      return new PodmanStackConfiguration()
-    }
 
 }
--- src/lib/drivers/podman/podman-socket-run-driver.ts
+++ src/lib/drivers/podman/podman-socket-run-driver.ts
@@ -2,16 +2,9 @@
 // Podman-Run-Driver: Controls Podman Socket For Running containers
 // ===========================================================================
 
-import {Dictionary} from '../abstract/run-driver'
 import {DockerSocketRunDriver} from '../docker/docker-socket-run-driver'
-import {PodmanStackConfiguration} from '../../config/stacks/podman/podman-stack-configuration'
 
 export class PodmanSocketRunDriver extends DockerSocketRunDriver
 {
-
-  emptyConfiguration()
-  {
-    return new PodmanStackConfiguration()
-  }
 
 }
