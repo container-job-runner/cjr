44de8dac5a3afa354dab4d8663efed6a092c127a
buvoli <buvoli@uw.edu>
2020-05-03 12:33:38 +0000

    updated to match new build-driver interface

--- src/lib/drivers/podman/podman-build-driver.ts
+++ src/lib/drivers/podman/podman-build-driver.ts
@@ -1,6 +1,6 @@
-import {DockerBuildDriver} from '../docker/docker-build-driver'
-import {PodmanStackConfiguration} from '../../config/stacks/podman/podman-stack-configuration'
+import { DockerBuildDriver } from '../docker/docker-build-driver'
 import { parseJSON } from '../../functions/misc-functions'
+import { DockerStackConfiguration } from '../../config/stacks/docker/docker-stack-configuration'
 
 // - types ---------------------------------------------------------------------
 type Dictionary = {[key: string]: any}
@@ -8,33 +8,29 @@ type Dictionary = {[key: string]: any}
 export class PodmanBuildDriver extends DockerBuildDriver
 {
     protected base_command = 'podman'
-    protected json_output_format = "json"
+    protected outputParser = parseJSON
 
-    isBuilt(stack_path: string, configuration:PodmanStackConfiguration)
+    isBuilt(configuration:DockerStackConfiguration)
     {
+      const image_name = configuration.getImage()
       const command = `${this.base_command} images`;
       const args:Array<string> = []
       const flags:Dictionary = {
-        filter: `reference=${this.imageName(stack_path, configuration.buildHash())}`
+        filter: `reference=${image_name}`
       }
       this.addJSONFormatFlag(flags);
-      var result = parseJSON(this.shell.output(command, flags, args, {}))
+      var result = this.outputParser(this.shell.output(command, flags, args, {}))
       if(!result.success) return false
       // extra logic since podman images --reference=name:tag is equivalent to docker images --reference=*name:tag
       return result.value.some((image_data:Dictionary) =>
         image_data.names.some((name: string) =>
-          (new RegExp(`/${this.imageName(stack_path, configuration.buildHash())}$`)).test(name)))
+          (new RegExp(`/${image_name}$`)).test(name)))
     }
 
     protected addJSONFormatFlag(flags: Dictionary)
     {
       flags["format"] = {shorthand: false, value: 'json'}
       return flags
-    }
-
-    emptyConfiguration()
-    {
-      return new PodmanStackConfiguration()
     }
 
 }
