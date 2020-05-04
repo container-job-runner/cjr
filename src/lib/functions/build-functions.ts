629fcdca35f26059e5729782fca9d3a014c1f276
buvoli <buvoli@uw.edu>
2020-05-04 11:59:14 +0000

    updated function so they adhere to new interface for BuildDriver and RunDriver

--- src/lib/functions/build-functions.ts
+++ src/lib/functions/build-functions.ts
@@ -2,6 +2,7 @@ import {BuildDriver} from '../drivers/abstract/build-driver'
 import {ValidatedOutput} from '../validated-output'
 import {ErrorStrings} from '../error-strings'
 import {StackConfiguration} from '../config/stacks/abstract/stack-configuration'
+import { ContainerRuntime } from './run-functions'
 
 export type BuildOptions = {
   'never'?: boolean,          // image will never be build
@@ -24,52 +25,55 @@ export type BuildOptions = {
 // stack_path - absolute path to stack folder
 // overloaded_config_paths - absolute paths to any overloading configuration files
 // -----------------------------------------------------------------------------
-export function buildAndLoad(builder: BuildDriver, build_options: BuildOptions, stack_path: string, overloaded_config_paths: Array<string>) : ValidatedOutput<StackConfiguration>
+export function buildAndLoad(cr: ContainerRuntime, build_options: BuildOptions, stack_path: string, overloaded_config_paths: Array<string>) : ValidatedOutput<StackConfiguration<any>>
 {
-  var result = builder.loadConfiguration(stack_path, overloaded_config_paths)
-  if(!result.success) return result
-  const configuration = result.value
+  const configuration = cr.runner.emptyStackConfiguration()
+  const result = new ValidatedOutput(true, configuration)
+
+  const load_result = configuration.load(stack_path, overloaded_config_paths)
+  if(!load_result.success) return result.absorb(load_result)
+
   if(build_options?.['never']) // simply return configuration
-    return new ValidatedOutput(true, configuration)
+    return result
   else if(build_options?.['reuse-image']) // build if image is missing
     result.absorb(
-      buildIfMissing(builder, stack_path, configuration, build_options)
+      buildIfMissing(cr.builder, stack_path, configuration, build_options)
     )
   else
     result.absorb(
-      builder.build(stack_path, configuration, build_options)
+      cr.builder.build(configuration, build_options)
     )
   if(!result.success) return result
   return new ValidatedOutput(true, configuration)
 }
 
-export function buildIfMissing(builder: BuildDriver, stack_path: string, configuration: StackConfiguration, build_options: BuildOptions) : ValidatedOutput<undefined>
+export function buildIfMissing(builder: BuildDriver, stack_path: string, configuration: StackConfiguration<any>, build_options: BuildOptions) : ValidatedOutput<undefined>
 {
-  if(builder.isBuilt(stack_path, configuration))
+  if(builder.isBuilt(configuration))
   {
     return new ValidatedOutput(true, undefined);
   }
   else
   {
-    const result = builder.build(stack_path, configuration, build_options)
+    const result = builder.build(configuration, build_options)
     if(result.success == true)
     {
-        result.success = builder.isBuilt(stack_path, configuration)
-        if(result.success == false) result.pushError(ErrorStrings.BUILD.FAILED_AUTOBUILD)
+      result.success = builder.isBuilt(configuration)
+      if(result.success == false) result.pushError(ErrorStrings.BUILD.FAILED_AUTOBUILD)
     }
     return result;
   }
 }
 
-export function removeImage(builder: BuildDriver, stack_path: string, all_configurations: boolean, overloaded_config_paths: Array<string>=[])
+export function removeImage(cr: ContainerRuntime, stack_path: string, all_configurations: boolean, overloaded_config_paths: Array<string>=[]) : ValidatedOutput<undefined>
 {
   if(all_configurations === true)
-    return builder.removeImage(stack_path)
+    return cr.builder.removeAllImages(stack_path)
   else
   {
-    var result = builder.loadConfiguration(stack_path, overloaded_config_paths)
-    if(!result.success) return result
-    const configuration = result.value
-    return builder.removeImage(stack_path, configuration)
+    const configuration = cr.runner.emptyStackConfiguration()
+    const load_result = configuration.load(stack_path, overloaded_config_paths)
+    if(!load_result.success) return load_result
+    return cr.builder.removeImage(configuration)
   }
 }
