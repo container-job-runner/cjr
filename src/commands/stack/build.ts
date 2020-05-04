96cd4e03b6bb81f99c78590446354386cd0ac490
buvoli <buvoli@uw.edu>
2020-05-04 11:25:50 +0000

    cleaned up flags

--- src/commands/stack/build.ts
+++ src/commands/stack/build.ts
@@ -9,13 +9,13 @@ export default class Build extends StackCommand {
   static description = 'Manually build images for one or more stacks.'
   static args = [{name: 'stack'}]
   static flags = {
-    stack: flags.string({env: 'STACK', multiple: true}),
+    "stack": flags.string({env: 'STACK', multiple: true}),
     "config-files": flags.string({default: [], multiple: true, description: "additional configuration file to override stack configuration"}),
-    explicit: flags.boolean({default: false}),
-    "quiet": flags.boolean({default: false, char: 'q'}),
+    "explicit": flags.boolean({default: false}),
     "no-cache": flags.boolean({default: false}),
     "pull": flags.boolean({default: false}),
-    "stacks-dir": flags.string({default: "", description: "override default stack directory"})
+    "stacks-dir": flags.string({default: "", description: "override default stack directory"}),
+    "quiet": flags.boolean({default: false, char: 'q'})
   }
   static strict = false;
 
@@ -25,7 +25,7 @@ export default class Build extends StackCommand {
     this.augmentFlagsWithProjectSettings(flags, {stack:false, "config-files": false, "stacks-dir": true})
     const stack_list = (argv.length > 0) ? argv : (JSTools.arrayWrap(flags.stack) || []) // add arrayWrap since parseWithLoad will return scalar
     const c_runtime:ContainerRuntime = {
-      builder: this.newBuilder(flags.explicit, !flags.verbose),
+      builder: this.newBuilder(flags.explicit, flags.quiet),
       runner:  this.newRunner(flags.explicit, flags.quiet)
     }
     stack_list.map((stack_name:string) => {
