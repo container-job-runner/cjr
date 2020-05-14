9f5a1f142d8ce1f2eb7b0bd2f977e554742d4fc9
buvoli <buvoli@uw.edu>
2020-05-14 08:08:46 +0000

    added image field to failure output for firstJob function

--- src/lib/drivers-containers/abstract/run-driver.ts
+++ src/lib/drivers-containers/abstract/run-driver.ts
@@ -150,7 +150,7 @@ export function firstJobId(job_info: ValidatedOutput<Array<JobInfo>>) : Validate
 
 export function firstJob(job_info: ValidatedOutput<Array<JobInfo>>) : ValidatedOutput<JobInfo>
 {
-  const failure_output:JobInfo = {id: "", names: [], command: "", status: "", state: "dead", stack: "", labels: {}, ports: []}
+  const failure_output:JobInfo = {id: "", image: "", names: [], command: "", status: "", state: "dead", stack: "", labels: {}, ports: []}
   if(job_info.value.length < 1)
     return new ValidatedOutput(false, failure_output)
   return new ValidatedOutput(true, job_info.value[0])
