0c48007e8060e09de936d1f71ad6629b94d87e94
buvoli <buvoli@uw.edu>
2020-05-16 12:54:57 +0000

    greatly simplified functions

--- src/lib/functions/build-functions.ts
+++ src/lib/functions/build-functions.ts
@@ -1,76 +1,31 @@
-import { BuildDriver } from '../drivers-containers/abstract/build-driver'
 import { ValidatedOutput } from '../validated-output'
-import { ErrorStrings } from '../error-strings'
 import { StackConfiguration } from '../config/stacks/abstract/stack-configuration'
-import { ContainerDrivers } from './run-functions'
 import { JobConfiguration } from '../config/jobs/job-configuration'
 import { NewJobInfo } from '../drivers-containers/abstract/run-driver'
+import { ContainerDrivers } from '../drivers-jobs/job-driver'
 
 export type BuildOptions = {
-  'never'?: boolean          // image will never be build
-  'reuse-image'?: boolean     // will not build if image with proper name already exists
-  'no-cache'?: boolean       // if true will build image without cache
-  'pull'?:  boolean           // if true will pull all linked images
+  'reuse-image': boolean     // will not build if image with proper name already exists
   'verbose'?: boolean
 }
 
-// -----------------------------------------------------------------------------
-// BUILDANDLOAD Calls function onSuccess if stack is build and successuffly
-//  loaded. The following arguments are passed to onSuccess
-//    1. configuration (Configuration) - the stack Configuration
-//    2. containerRoot - the container project root folder
-//    3. hostRoot (String | false) - the project hostRoot or false if non existsSync
-// -- Parameters ---------------------------------------------------------------
-// builder  - (BuildDriver) Object that inherits from abstract class Configuration
-// flags    - (Object) command flags. The only optional propertes will affect this function are:
-//              1. containerRoot
-//              2. hostRoot
-// stack_path - absolute path to stack folder
-// overloaded_config_paths - absolute paths to any overloading configuration files
-// -----------------------------------------------------------------------------
-export function buildAndLoad(cr: ContainerDrivers, build_options: BuildOptions, stack_path: string, overloaded_config_paths: Array<string>) : ValidatedOutput<StackConfiguration<any>>
-{
-  const configuration = cr.runner.emptyStackConfiguration()
-  const result = new ValidatedOutput(true, configuration)
-
-  const load_result = configuration.load(stack_path, overloaded_config_paths)
-  if(!load_result.success) return result.absorb(load_result)
-
-  if(build_options?.['never']) // simply return configuration
-    return result
-  else if(build_options?.['reuse-image']) // build if image is missing
-    result.absorb(
-      buildIfMissing(cr.builder, configuration, build_options)
-    )
-  else
-    result.absorb(
-      cr.builder.build(configuration, (build_options.verbose) ? "inherit" : "pipe", build_options)
-    )
-  if(!result.success) return result
-  return new ValidatedOutput(true, configuration)
-}
-
-export function buildIfMissing(builder: BuildDriver, configuration: StackConfiguration<any>, build_options: BuildOptions) : ValidatedOutput<undefined>
+export function buildImage(configuration: StackConfiguration<any>, drivers: ContainerDrivers, build_options: BuildOptions) : ValidatedOutput<undefined>
 {
   const result = new ValidatedOutput(true, undefined)
-  if(!builder.isBuilt(configuration))
-  {
-    result.absorb(builder.build(configuration, (build_options.verbose) ? "inherit" : "pipe", build_options))
-    if(!result.success) return result
-    if(!builder.isBuilt(configuration)) result.pushError(ErrorStrings.BUILD.FAILED_AUTOBUILD)
-  }
-  return result;
+  if(build_options["reuse-image"] && drivers.builder.isBuilt(configuration))
+    return result
+  else
+    return result.absorb(
+      drivers.builder.build(configuration, (build_options.verbose) ? "inherit" : "pipe", build_options)
+    )
 }
 
-export function removeImage(cr: ContainerDrivers, stack_path: string, all_configurations: boolean, overloaded_config_paths: Array<string>=[]) : ValidatedOutput<undefined>
+export function buildAndRun(job_configuration: JobConfiguration<StackConfiguration<any>>, drivers: ContainerDrivers, build_options:BuildOptions) : ValidatedOutput<NewJobInfo>
 {
-  if(all_configurations === true)
-    return cr.builder.removeAllImages(stack_path)
-  else
-  {
-    const configuration = cr.runner.emptyStackConfiguration()
-    const load_result = configuration.load(stack_path, overloaded_config_paths)
-    if(!load_result.success) return load_result
-    return cr.builder.removeImage(configuration)
-  }
+  const failed_result = new ValidatedOutput(false, {"id": "", "exit-code": 0, "output": ""});
+  const build_result = buildImage(job_configuration.stack_configuration, drivers, build_options)
+  if(!build_result.success)
+    return failed_result
+  return drivers.runner.jobStart(job_configuration, "inherit")
 }
+
