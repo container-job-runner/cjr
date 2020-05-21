17dc797abde9f4814de242f7d813f573052e1e49
buvoli <buvoli@uw.edu>
2020-05-21 01:42:07 +0000

    added server command for jupyter and theia

---
+++ src/lib/commands/server-command.ts
@@ -0,0 +1,20 @@
+import { NewJobCommand } from './new-job-command'
+import { ContainerDrivers } from '../job-managers/job-manager'
+import { nextAvailablePort } from '../functions/cli-functions'
+
+export abstract class ServerCommand extends NewJobCommand
+{
+  defaultPort(drivers: ContainerDrivers, server_port_flag: string, expose: boolean)
+  {
+    const default_address = (expose) ? '0.0.0.0' : '127.0.0.1'
+    const port = this.parsePortFlag([server_port_flag]).pop()
+    if(port !== undefined && port.address)
+      return port
+    if(port !== undefined) {
+      port.address = default_address
+      return port
+    }
+    const default_port = nextAvailablePort(drivers, 7019)
+    return {"hostPort": default_port, "containerPort": default_port, "address": default_address}
+  }
+}
