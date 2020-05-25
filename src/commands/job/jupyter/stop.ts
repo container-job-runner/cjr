071004c64e2968682118b4b6d094b01de4edcd9e
buvoli <buvoli@uw.edu>
2020-05-25 11:01:45 +0000

    added flag all

--- src/commands/job/jupyter/stop.ts
+++ src/commands/job/jupyter/stop.ts
@@ -1,7 +1,8 @@
 import { flags } from '@oclif/command'
 import { printResultState } from '../../../lib/functions/misc-functions'
 import { BasicCommand } from '../../../lib/commands/basic-command'
-import { stopJupyter } from '../../../lib/functions/jupyter-functions'
+import { stopJupyter, stopAllJupyters } from '../../../lib/functions/jupyter-functions'
+import { ValidatedOutput } from '../../../lib/validated-output'
 
 export default class Stop extends BasicCommand {
   static description = 'Stop a running Jupyter server.'
@@ -9,6 +10,7 @@ export default class Stop extends BasicCommand {
   static flags = {
     "project-root": flags.string({env: 'PROJECTROOT'}),
     "here": flags.boolean({default: false, char: 'h', exclusive: ['project-root'], description: 'sets project-root to current working directory'}),
+    "all": flags.boolean({description: "stop all jupyter servers running in host directories"}),
     "verbose": flags.boolean({default: false, char: 'v', description: 'shows output for each stage of the job.', exclusive: ['quiet']}),
     "quiet": flags.boolean({default: false, char: 'q'}),
     "explicit": flags.boolean({default: false}),
@@ -22,13 +24,18 @@ export default class Stop extends BasicCommand {
     const { args, flags } = this.parse(Stop)
     this.augmentFlagsWithProjectSettings(flags, {"project-root": false})
     this.augmentFlagsWithHere(flags)
+    const { job_manager } = this.initContainerSDK(flags['verbose'], flags['quiet'], flags['explicit'])
+
+    if(flags['all'])
+      return printResultState(
+        stopAllJupyters(job_manager, "in-job")
+      )
     // -- get job ids --------------------------------------------------------
     const job_id = await this.getJobId([args['id']], flags)
     if(job_id === false) return // exit if user selects empty id or exits interactive dialog
-
-    const { job_manager } = this.initContainerSDK(flags['verbose'], flags['quiet'], flags['explicit'])
-    const result = stopJupyter(job_manager, {"job-id": job_id});
-    printResultState(result)
+    printResultState(
+      stopJupyter(job_manager, {"job-id": job_id})
+    )
   }
 
 }
--- src/commands/jupyter/stop.ts
+++ src/commands/jupyter/stop.ts
@@ -1,13 +1,15 @@
 import { flags } from '@oclif/command'
 import { printResultState } from '../../lib/functions/misc-functions'
 import { BasicCommand } from '../../lib/commands/basic-command'
-import { stopJupyter } from '../../lib/functions/jupyter-functions'
+import { stopJupyter, stopAllJupyters } from '../../lib/functions/jupyter-functions'
+import { ValidatedOutput } from '../../lib/validated-output'
 
 export default class Stop extends BasicCommand {
   static description = 'Stop a running Jupyter server.'
   static flags = {
     "project-root": flags.string({env: 'PROJECTROOT'}),
     "here": flags.boolean({default: false, char: 'h', exclusive: ['project-root'], description: 'sets project-root to current working directory'}),
+    "all": flags.boolean({description: "stop all jupyter servers running in host directories"}),
     "verbose": flags.boolean({default: false, char: 'v', description: 'shows output for each stage of the job.', exclusive: ['quiet']}),
     "quiet": flags.boolean({default: false, char: 'q'}),
     "explicit": flags.boolean({default: false})
@@ -21,9 +23,13 @@ export default class Stop extends BasicCommand {
     this.augmentFlagsWithHere(flags)
 
     const { job_manager } = this.initContainerSDK(flags['verbose'], flags['quiet'], flags['explicit'])
-    const result = stopJupyter(job_manager, {
-      "project-root": flags['project-root']
-    });
+    let result:ValidatedOutput<undefined>;
+    if(flags['all'])
+      result = stopAllJupyters(job_manager, "in-project")
+    else
+      result = stopJupyter(job_manager, {
+        "project-root": flags['project-root']
+      });
     printResultState(result)
   }
 
