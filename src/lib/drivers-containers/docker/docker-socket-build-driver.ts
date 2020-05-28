c3cb47b05aec19e1a15c8b9a53a60babad9c4647
buvoli <buvoli@uw.edu>
2020-05-28 12:34:08 +0000

    refactored API calls to separate functions

--- src/lib/drivers-containers/docker/docker-socket-build-driver.ts
+++ src/lib/drivers-containers/docker/docker-socket-build-driver.ts
@@ -19,6 +19,7 @@ export class DockerSocketBuildDriver extends BuildDriver
   protected tmpdir: string // path to tmp directory for saving tar files
   protected curl: Curl
   protected curlPostProcessor = DockerAPIPostProcessor
+  protected base_url = "http://v1.24" // base url for api
 
   protected ERRORSTRINGS = {
     "INVALID_CONFIGURATION": chalk`{bold Invalid Configuration} - This build driver requires a DockerStackConfiguration.`,
@@ -28,7 +29,8 @@ export class DockerSocketBuildDriver extends BuildDriver
     "FAILED_TO_BUILD": (stack: string) => chalk`{bold Image Build Failed} - stack configuration ${stack} likely contains errors.`,
     "FAILED_TO_LOAD": (file:string) => chalk`{bold Image Load Failed} - failed to load ${file}.`,
     "FAILED_TO_DELETE": (id:string) => chalk`{bold Image Remove Failed} - could not remove image ${id}.`,
-    "FAILED_TO_LISTIMAGES": chalk`{bold Image List Failed} - could not get list of current images.`
+    "FAILED_TO_LISTIMAGES": chalk`{bold Image List Failed} - could not get list of current images.`,
+    "FAILED_TO_TAG": (id:string) => chalk`{bold Image Tag Failed} - could not tag image ${id}.`
   }
 
   constructor(shell: ShellCommand, options: {socket: string, tmpdir: string})
@@ -38,30 +40,15 @@ export class DockerSocketBuildDriver extends BuildDriver
     this.tmpdir = options.tmpdir
     this.curl = new Curl(shell, {
       "unix-socket": options.socket,
-      "base-url": "http://v1.24"
+      "base-url": this.base_url
     })
   }
 
   isBuilt(configuration: StackConfiguration<any>): boolean
   {
     // -- make api request -----------------------------------------------------
-    const api_result = this.curlPostProcessor(
-      this.curl.get({
-        "url": "/images/json",
-        "params": {
-          "filters": JSON.stringify({
-            "reference": [configuration.getImage()]
-          })
-        }
-      })
-    )
-
-    // -- check request status -------------------------------------------------
-    if(!this.validJSONAPIResponse(api_result, 200))
-      return false
-
-    // -- return true if -------------------------------------------------------
-    if(api_result.value.body?.length > 0)
+    const result = this.API_ImageListWithReference(configuration.getImage())
+    if(result.success && result.value.length > 0)
       return true
     return false
   }
@@ -105,53 +92,34 @@ export class DockerSocketBuildDriver extends BuildDriver
 
   protected loadArchivedImage(configuration: DockerStackConfiguration, options?: Dictionary) : ValidatedOutput<string>
   {
-    // -- exit with failure if stack is not of correct type
+    // -- exit with failure if stack is not of correct type --------------------
     const result = new ValidatedOutput(true, "")
     if(!configuration.stack_path)
       return result.pushError(this.ERRORSTRINGS.FAILED_TO_BUILD(""))
     if(!['tar', 'tar.gz'].includes(configuration.stack_type as string))
       return result.pushError(this.ERRORSTRINGS.FAILED_TO_BUILD(configuration.stack_path))
 
-    // only extract tar if image does not exist, or if no-cache is specified
+    // -- only extract tar if image does not exist, or if no-cache is true -----
     if(this.isBuilt(configuration) && !(configuration?.config?.build?.["no-cache"] || options?.['no-cache']))
       return result
 
     // -- make api request -----------------------------------------------------
-    const archive = path.join(configuration.stack_path, `${configuration.archive_filename}.${configuration.stack_type}`)
-    const encoding = (configuration.stack_type == 'tar.gz') ? 'gzip' : 'tar'
-    const api_load_result = this.curlPostProcessor(
-      this.curl.post({
-        "url": "/images/load",
-        "encoding": encoding,
-        "params": {},
-        "file": archive
-      })
+    const archive = path.join(
+      configuration.stack_path,
+      `${configuration.archive_filename}.${configuration.stack_type}`
     )
-    result.value = this.chunkedJsonStreamStr(api_load_result.value.body)
-
-    // -- check request status -------------------------------------------------
-    if(!this.validJSONAPIResponse(api_load_result, 200))
-      result.pushError(this.ERRORSTRINGS.FAILED_TO_LOAD(archive))
-
-    // -- extract id -----------------------------------------------------------
-    const id:string = this.filterStreamData(api_load_result.value.body).pop()?.stream?.match(/(?<=sha256:)[a-zA-z0-9]+/)?.pop() || ""
-    if(!id) return result.pushError(this.ERRORSTRINGS.FAILED_TO_EXTRACT_IMAGE_ID(archive));
+    const encoding = (configuration.stack_type == 'tar.gz') ? 'gzip' : 'tar'
+    const api_result = this.API_LoadArchive(archive, encoding)
+    result.value = api_result.value.output
+    if(!api_result.success)
+      return result.absorb(api_result)
 
     // -- retag image ----------------------------------------------------------
-    const [repo, tag] = configuration.getImage().split(":")
-    const api_tag_result = this.curlPostProcessor(
-      this.curl.post({
-        "url": `/images/${id}/tag`,
-        "encoding": "json",
-        "params": {
-          "repo": repo,
-          "tag": tag
-        }
-      })
-    )
+    const id = api_result.value.id
+    const [ repo, tag ] = configuration.getImage().split(":")
+    const api_tag_result = this.API_TagImage(id, repo, tag)
 
-    // -- check request status -------------------------------------------------
-    if(!this.validAPIResponse(api_tag_result, 201))
+    if(!api_tag_result.success)
       result.pushError(this.ERRORSTRINGS.FAILED_TO_LOAD(archive))
 
     return result
@@ -171,20 +139,11 @@ export class DockerSocketBuildDriver extends BuildDriver
       return result
 
     // -- make api request -----------------------------------------------------
-    const pull_result = this.curlPostProcessor(
-      this.curl.post({
-        "url": "/images/create",
-        "params": {
-          "fromImage": configuration.getImage()
-        }
-      })
-    )
+    const pull_result = this.API_PullImage(configuration.getImage())
+    result.value = pull_result.value.output
 
-    result.value = this.chunkedJsonStreamStr(pull_result.value.body)
     result.absorb(pull_result)
     // -- check request status -------------------------------------------------
-    if(!this.validJSONAPIResponse(pull_result, 200))
-      result.pushError(this.ERRORSTRINGS.FAILED_TO_PULL(configuration.getImage()))
     return result
 
   }
@@ -198,11 +157,11 @@ export class DockerSocketBuildDriver extends BuildDriver
     if(configuration.stack_type !== 'dockerfile')
       return result.pushError(this.ERRORSTRINGS.FAILED_TO_BUILD(configuration.stack_path))
 
-    // -- 1. make temporary directory ----------------------------------------------
+    // -- 1. make temporary directory -------------------------------------------
     const mkdir = FileTools.mktempDir(this.tmpdir, this.shell)
     if(!mkdir.success) return result.absorb(mkdir)
     const tmp_dir = mkdir.value
-    // -- 2. tar contents of stack build folder into build.tar.gz ------------------
+    // -- 2. tar contents of stack build folder into build.tar.gz ---------------
     const build_path = path.join(configuration.stack_path, configuration.build_context)
     const archive_name = path.join(tmp_dir, 'build.tar.gz')
     const tar = this.shell.exec(
@@ -215,8 +174,8 @@ export class DockerSocketBuildDriver extends BuildDriver
       fs.removeSync(tmp_dir)
       return result.absorb(tar)
     }
-    // -- 3. call api to build Archive ---------------------------------------------
-    const build_result = this.buildArchive({
+    // -- 3. call api to build Archive ------------------------------------------
+    const build_result = this.API_Build({
       "archive": archive_name,
       "imageName": configuration.getImage(),
       "buildargs": configuration.getBuildArgs(),
@@ -224,15 +183,72 @@ export class DockerSocketBuildDriver extends BuildDriver
       "pull": options?.pull || false,
       "nocache": options?.nocache || false
     })
-    result.value = this.chunkedJsonStreamStr(build_result.value.body)
     result.absorb(build_result)
-    // -- 4. remove tmp folder -----------------------------------------------------
+    result.value = build_result.value.output
+    // -- 4. remove tmp folder -------------------------------------------------
     fs.removeSync(tmp_dir)
     return result
   }
 
-  protected buildArchive(options: {archive: string, imageName?: string, encoding: "tar"|"gzip", buildargs?: {[key: string] : string}, pull?: boolean, nocache?: boolean}) : ValidatedOutput<RequestOutput>
+  removeImage(configuration: DockerStackConfiguration): ValidatedOutput<undefined>
   {
+    return this.API_RemoveImage([configuration.getImage()])
+  }
+
+  removeAllImages(stack_path: string): ValidatedOutput<undefined>
+  {
+    // -- make api request -----------------------------------------------------
+    const result = new ValidatedOutput(true, undefined)
+    const api_list = this.API_ImageListWithStackLabel(stack_path)
+    result.absorb(api_list, this.API_RemoveImage(api_list.value))
+    return result
+  }
+
+  // ===========================================================================
+  // API Output Processing functions
+  // ===========================================================================
+
+  // turns chucked output tagged with stream into a string
+  protected streamsToString(body: Array<{stream: string}>) : string
+  {
+    return this.extractStreamStrings(body).join("")
+  }
+
+  // certain Docker API endpoints returns a json object on each line
+  // this function parses objects that contain the field {stream : ""}
+  protected extractStreamStrings(body: any) : Array<string>
+  {
+    if(!JSTools.isArray(body))
+      return []
+    const stream: Array<string> = []
+    body?.map( (a:any) => {
+      if(typeof a?.stream === "string")
+        stream.push(a.stream)
+    })
+    return stream
+  }
+
+  protected validAPIResponse(response: ValidatedOutput<RequestOutput>, code?:number) : boolean
+  {
+    if(!response.success) return false
+    if(code !== undefined && response.value.header.code !== code) return false
+    return true
+  }
+
+  protected validJSONAPIResponse(response: ValidatedOutput<RequestOutput>, code?:number) : boolean
+  {
+    if(!this.validAPIResponse(response, code)) return false
+    if(response.value.header.type !== "application/json") return false
+    return true
+  }
+
+  // ===========================================================================
+  // Function Wrappers for API calls
+  // ===========================================================================
+
+  protected API_Build(options: {archive: string, imageName?: string, encoding: "tar"|"gzip", buildargs?: {[key: string] : string}, pull?: boolean, nocache?: boolean}) : ValidatedOutput<{output: string}>
+  {
+    const result = new ValidatedOutput(true, {output: ""})
     const build_result = this.curlPostProcessor(
       this.curl.post({
         "url": `/build`,
@@ -250,26 +266,117 @@ export class DockerSocketBuildDriver extends BuildDriver
 
     // -- check request status -------------------------------------------------
     if(!this.validAPIResponse(build_result, 200))
-      build_result.pushError(this.ERRORSTRINGS.FAILED_TO_BUILD(options.archive))
+      result.pushError(this.ERRORSTRINGS.FAILED_TO_BUILD(options.archive))
 
-    return build_result
+    result.value.output = this.streamsToString(build_result.value.body)
+    return result
   }
 
-  removeImage(configuration: DockerStackConfiguration): ValidatedOutput<undefined>
+  protected API_LoadArchive(archive: string, encoding: 'tar'|'gzip') : ValidatedOutput<{id: string, output: string}>
   {
-    return this.rmi(configuration.getImage())
+    const result = new ValidatedOutput(true, {id: "", output: ""})
+    const api_load_result = this.curlPostProcessor(
+      this.curl.post({
+        "url": "/images/load",
+        "encoding": encoding,
+        "params": {},
+        "file": archive
+      })
+    )
+
+    // -- check request status -------------------------------------------------
+    if(!this.validJSONAPIResponse(api_load_result, 200))
+      result.pushError(this.ERRORSTRINGS.FAILED_TO_LOAD(archive))
+
+    // -- extract output and id ------------------------------------------------
+    const output = this.streamsToString(api_load_result.value.body)
+    const id = this.API_ExtractLoadImageId(output)
+    if(!id)
+      result.pushError(this.ERRORSTRINGS.FAILED_TO_EXTRACT_IMAGE_ID(archive));
+
+    result.value.output = output
+    result.value.id = id
+    return result
   }
 
-  removeAllImages(stack_path: string): ValidatedOutput<undefined>
+  protected API_ExtractLoadImageId(load_output: string) : string
+  {
+    return load_output.match(/(?<=sha256:)[a-zA-z0-9]+/)?.pop() || ""
+  }
+
+  protected API_TagImage(id: string, repo: string, tag: string) : ValidatedOutput<undefined>
   {
     const result = new ValidatedOutput(true, undefined)
+    const api_tag_result = this.curlPostProcessor(
+      this.curl.post({
+        "url": `/images/${id}/tag`,
+        "encoding": "json",
+        "params": {
+          "repo": repo,
+          "tag": tag
+        }
+      })
+    )
+
+    // -- check request status -------------------------------------------------
+    if(!this.validAPIResponse(api_tag_result, 201))
+      result.pushError(this.ERRORSTRINGS.FAILED_TO_TAG(id))
+
+    return result
+
+  }
+
+  protected API_PullImage(image_reference: string) : ValidatedOutput<{output: string}>
+  {
+    const result = new ValidatedOutput(true, {output: ""})
+    const pull_result = this.curlPostProcessor(
+      this.curl.post({
+        "url": "/images/create",
+        "params": {
+          "fromImage": image_reference
+        }
+      })
+    )
+    if(!this.validJSONAPIResponse(pull_result, 200))
+      pull_result.pushError(this.ERRORSTRINGS.FAILED_TO_PULL(image_reference))
+
+    result.value.output = this.streamsToString(pull_result.value.body)
+    return result
+  }
+
+  protected API_RemoveImage(ids: Array<string>) : ValidatedOutput<undefined>
+  {
+    const result = new ValidatedOutput(true, undefined)
+
+    // -- make api request -----------------------------------------------------
+    ids.map((id:string) => {
+      const api_result = this.curlPostProcessor(
+        this.curl.delete({
+          "url": `/images/${id}`,
+          "params": {}
+        })
+      )
+      result.absorb(api_result)
+      if(!this.validJSONAPIResponse(api_result, 200))
+        result.pushError(this.ERRORSTRINGS.FAILED_TO_DELETE(id))
+    })
+
+    return result
+  }
+
+  protected API_ImageListWithStackLabel(stack_path: string) : ValidatedOutput<string[]>
+  {
+    const result = new ValidatedOutput(true, [])
 
     // -- make api request -----------------------------------------------------
     const api_list = this.curlPostProcessor(
       this.curl.get({
         "url": `/images/json`,
         "params": {
-          "filters": JSON.stringify({"label": [`builder=${cli_name}`, `stack=${stack_path}`]})
+          "filters": JSON.stringify({"label": [
+            `builder=${cli_name}`,
+            `stack=${stack_path}`
+          ]})
         }
       })
     )
@@ -278,65 +385,36 @@ export class DockerSocketBuildDriver extends BuildDriver
     if(!this.validJSONAPIResponse(api_list, 200))
       return result.pushError(this.ERRORSTRINGS.FAILED_TO_LISTIMAGES)
 
-    const image_ids = api_list.value.body
+    result.value = api_list.value.body
       ?.map((o:any):string => (typeof o?.Id == "string") ? o?.Id : "")
       ?.filter((s:string) => s != "") || []
-
-    image_ids.map((id:string) => {
-      result.absorb(this.rmi(id))
-    })
-
     return result
+
   }
 
-  private rmi(id: string) : ValidatedOutput<undefined>
+  protected API_ImageListWithReference(reference: string): ValidatedOutput<string[]>
   {
-    const result = new ValidatedOutput(true, undefined)
+    const result = new ValidatedOutput(true, [])
 
     // -- make api request -----------------------------------------------------
     const api_result = this.curlPostProcessor(
-      this.curl.delete({
-        "url": `/images/${id}`,
-        "params": {}
+      this.curl.get({
+        "url": "/images/json",
+        "params": {
+          "filters": JSON.stringify({
+            "reference": [reference]
+          })
+        }
       })
     )
 
     // -- check request status -------------------------------------------------
-    if(!this.validJSONAPIResponse(api_result, 200))
-      result.pushError(this.ERRORSTRINGS.FAILED_TO_DELETE(id))
+    if(!this.validJSONAPIResponse(api_result, 200) || !JSTools.isArray(api_result.value.body))
+      return result.pushError(this.ERRORSTRINGS.FAILED_TO_LISTIMAGES)
 
+    // -- return true if -------------------------------------------------------
+    result.value = api_result.value.body
     return result
-  }
-
-  private filterStreamData(body: any) : Array<{stream: string}>
-  {
-    if(!JSTools.isArray(body))
-      return []
-    const stream: Array<{stream: string}> = []
-    body?.map( (a:any) => {
-      if(typeof a?.stream === "string")
-        stream.push({"stream" : a.stream})
-    })
-    return stream
-  }
-
-  private chunkedJsonStreamStr(body: any) : string
-  {
-    return this.filterStreamData(body).map((o: {stream: string}) => o.stream).join("")
-  }
-
-  private validAPIResponse(response: ValidatedOutput<RequestOutput>, code?:number) : boolean
-  {
-    if(!response.success) return false
-    if(code !== undefined && response.value.header.code !== code) return false
-    return true
-  }
-
-  private validJSONAPIResponse(response: ValidatedOutput<RequestOutput>, code?:number) : boolean
-  {
-    if(!this.validAPIResponse(response, code)) return false
-    if(response.value.header.type !== "application/json") return false
-    return true
   }
 
 }
