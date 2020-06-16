612b684554e8beb27f01dcb538084232a351d97a
buvoli <buvoli@uw.edu>
2020-06-16 01:21:52 +0000

    command now only builds one stack and accepts profiles

--- src/commands/stack/build.ts
+++ src/commands/stack/build.ts
@@ -1,28 +1,33 @@
 import { flags } from '@oclif/command'
-import { BasicCommand } from '../../lib/commands/basic-command'
 import { JSTools } from '../../lib/js-tools'
 import { printValidatedOutput } from '../../lib/functions/misc-functions'
 import { buildImage } from '../../lib/functions/build-functions'
+import { BasicCommand } from '../../lib/commands/basic-command'
 
 export default class Build extends BasicCommand {
-  static description = 'Manually build images for one or more stacks.'
+  static description = 'Manually build an image for a stack.'
   static args = [{name: 'stack'}]
   static flags = {
-    "stack": flags.string({env: 'STACK', multiple: true}),
+    "stack": flags.string({env: 'STACK'}),
     "config-files": flags.string({default: [], multiple: true, description: "additional configuration file to override stack configuration"}),
     "explicit": flags.boolean({default: false}),
     "no-cache": flags.boolean({default: false}),
     "pull": flags.boolean({default: false}),
     "stacks-dir": flags.string({default: "", description: "override default stack directory"}),
+    "project-root": flags.string({env: 'PROJECTROOT'}),
+    "profile": flags.string({multiple: true, description: "set stack profile"}),
     "quiet": flags.boolean({default: false, char: 'q'})
   }
-  static strict = false;
+  static strict = true;
 
   async run()
   {
-    const {argv, flags} = this.parse(Build)
+    const {args, flags} = this.parse(Build)
     this.augmentFlagsWithProjectSettings(flags, {"stack": false, "config-files": false, "stacks-dir": true})
-    const stack_list = (argv.length > 0) ? argv : (JSTools.arrayWrap(flags.stack) || []) // add arrayWrap since parseWithLoad will return scalar
+    flags["stack"] = args?.['stack'] || flags["stack"]
+    this.augmentFlagsWithProfile(flags)
+
+    const stack_list = (flags.stack) ? [ flags.stack ] : []
     const { container_drivers, configurations } = this.initContainerSDK(true, flags.quiet, flags.explicit)
     stack_list.map((stack_name:string) => {
       const init_stack = this.initStackConfiguration({
