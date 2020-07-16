babda05fe28bb5a1de50f8d1c4771a299c87619b
buvoli <buvoli@uw.edu>
2020-07-16 00:51:51 +0000

    Added functions to ServerCommand for managing remote resources

--- src/lib/commands/server-command.ts
+++ src/lib/commands/server-command.ts
@@ -1,10 +1,13 @@
 import path = require('path')
 import { LocalJobCommand } from './local-job-command'
-import { ContainerDrivers } from '../job-managers/abstract/job-manager'
+import { ContainerDrivers, JobManager } from '../job-managers/abstract/job-manager'
 import { nextAvailablePort } from '../functions/cli-functions'
+import { RemoteSshJobManager } from '../job-managers/remote/remote-ssh-job-manager'
 
 export abstract class ServerCommand extends LocalJobCommand
 {
+    readonly localhost_ip = '127.0.0.1'
+    
     defaultPort(drivers: ContainerDrivers, server_port_flag: string, expose: boolean)
     {
         const default_address = (expose) ? '0.0.0.0' : '127.0.0.1'
@@ -24,4 +27,36 @@ export abstract class ServerCommand extends LocalJobCommand
         if( args['project-root'] && !flags['project-root'] ) // allow arg to override flag
             flags['project-root'] = path.resolve(args['project-root'])
     }
+
+    // functions for remote servers
+
+    getAccessIp(job_manager: JobManager, flags?: {tunnel?: boolean, resource?: string})
+    {
+        if(!flags?.resource)
+            return this.localhost_ip
+        
+        if(job_manager instanceof RemoteSshJobManager) {
+            if(flags?.['tunnel'])
+                return this.localhost_ip
+            return this.resource_configuration.getResource(flags['resource'])?.address || '0.0.0.0'
+        }
+        return this.localhost_ip
+    }
+
+    startTunnel(job_manager: JobManager, options: {port: number})
+    {
+        if(!(job_manager instanceof RemoteSshJobManager))
+            return
+        
+        job_manager.shell.tunnelStart({
+            "localIP": this.localhost_ip,
+            "remotePort": options.port,
+            "localPort": options.port,
+            "multiplex": {
+                "controlpersist" : 600,
+                "reuse-connection": true
+            }
+        })
+    }
+
 }
