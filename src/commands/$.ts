e2d08a6202bd2ca45c1b73f740752736697417a5
buvoli <buvoli@uw.edu>
2020-07-19 21:17:10 +0000

    Added job:start command (which is equivalent to the $ command)

--- src/commands/$.ts
+++ src/commands/$.ts
@@ -1,39 +1,17 @@
-import { flags } from '@oclif/command'
-import { printValidatedOutput } from '../lib/functions/misc-functions'
-import { LocalJobCommand } from '../lib/commands/local-job-command'
+import { Start } from './job/start'
 import { initX11 } from '../lib/functions/cli-functions'
+import { LocalJobCommand } from '../lib/commands/local-job-command'
+import { printValidatedOutput } from '../lib/functions/misc-functions'
 
-export default class Run extends LocalJobCommand {
-  static description = 'Start a job that runs a shell command.'
-  static args = [{name: 'command', required: true}]
-  static flags = {
-    "resource": flags.string({env: 'RESOURCE'}),
-    "stack": flags.string({env: 'STACK'}),
-    "profile": flags.string({multiple: true, description: "set stack profile"}),
-    "project-root": flags.string({env: 'PROJECTROOT'}),
-    "here": flags.boolean({default: false, exclusive: ['project-root'], description: 'sets project-root to current working directory'}),
-    "config-files": flags.string({default: [], multiple: true, description: "additional configuration file to override stack configuration"}),
-    "explicit": flags.boolean({default: false}),
-    "verbose": flags.boolean({default: false, char: 'v', description: 'shows output for each stage of the job.', exclusive: ['quiet']}),
-    "quiet": flags.boolean({default: false, char: 'q'}),
-    "async": flags.boolean({exclusive: ['sync']}),
-    "sync": flags.boolean({exclusive: ['async']}),
-    "port": flags.string({default: [], multiple: true}),
-    "x11": flags.boolean({default: false}),
-    "message": flags.string({description: "use this flag to tag a job with a user-supplied message"}),
-    "label": flags.string({default: [], multiple: true, description: "additional labels to append to job"}),
-    "autocopy": flags.boolean({default: false, exclusive: ["async"], description: "automatically copy files back to the projec root on exit"}),
-    "file-access": flags.string({default: "volume", options: ["volume", "bind"], description: "how files are accessed from the container. Options are: volume and bind."}),
-    "build-mode":  flags.string({default: "cached", description: 'specify how to build stack. Options include "reuse-image", "cached", "no-cache", "cached,pull", and "no-cache,pull"'}),
-    "no-autoload": flags.boolean({default: false, description: "prevents cli from automatically loading flags using project settings files"}),
-    "stacks-dir": flags.string({default: "", description: "override default stack directory"}),
-    "working-directory": flags.string({default: process.cwd(), description: 'cli will behave as if it was called from the specified directory'})
-  }
-  static strict = false;
+export class StartShortcut extends LocalJobCommand {
+  static description = Start.description
+  static args = Start.args
+  static flags = Start.flags
+  static strict = Start.strict;
 
   async run()
   {
-    const {argv, flags} = this.parse(Run)
+    const {argv, flags} = this.parse(Start)
     // -- check x11 user settings ----------------------------------------------
     if(flags['x11']) await initX11({
             'interactive': this.settings.get('interactive'),
---
+++ src/commands/job/start.ts
@@ -0,0 +1,50 @@
+import { flags } from '@oclif/command'
+import { printValidatedOutput } from '../../lib/functions/misc-functions'
+import { LocalJobCommand } from '../../lib/commands/local-job-command'
+import { initX11 } from '../../lib/functions/cli-functions'
+
+export class Start extends LocalJobCommand {
+  static description = 'Start a job that runs a shell command.'
+  static args = [{name: 'command', required: true}]
+  static flags = {
+    "resource": flags.string({env: 'RESOURCE'}),
+    "stack": flags.string({env: 'STACK'}),
+    "profile": flags.string({multiple: true, description: "set stack profile"}),
+    "project-root": flags.string({env: 'PROJECTROOT'}),
+    "here": flags.boolean({default: false, exclusive: ['project-root'], description: 'sets project-root to current working directory'}),
+    "config-files": flags.string({default: [], multiple: true, description: "additional configuration file to override stack configuration"}),
+    "explicit": flags.boolean({default: false}),
+    "verbose": flags.boolean({default: false, char: 'v', description: 'shows output for each stage of the job.', exclusive: ['quiet']}),
+    "quiet": flags.boolean({default: false, char: 'q'}),
+    "async": flags.boolean({exclusive: ['sync']}),
+    "sync": flags.boolean({exclusive: ['async']}),
+    "port": flags.string({default: [], multiple: true}),
+    "x11": flags.boolean({default: false}),
+    "message": flags.string({description: "use this flag to tag a job with a user-supplied message"}),
+    "label": flags.string({default: [], multiple: true, description: "additional labels to append to job"}),
+    "autocopy": flags.boolean({default: false, exclusive: ["async"], description: "automatically copy files back to the projec root on exit"}),
+    "file-access": flags.string({default: "volume", options: ["volume", "bind"], description: "how files are accessed from the container. Options are: volume and bind."}),
+    "build-mode":  flags.string({default: "cached", description: 'specify how to build stack. Options include "reuse-image", "cached", "no-cache", "cached,pull", and "no-cache,pull"'}),
+    "no-autoload": flags.boolean({default: false, description: "prevents cli from automatically loading flags using project settings files"}),
+    "stacks-dir": flags.string({default: "", description: "override default stack directory"}),
+    "working-directory": flags.string({default: process.cwd(), description: 'cli will behave as if it was called from the specified directory'})
+  }
+  static strict = false;
+
+  async run()
+  {
+    const {argv, flags} = this.parse(Start)
+    // -- check x11 user settings ----------------------------------------------
+    if(flags['x11']) await initX11({
+            'interactive': this.settings.get('interactive'),
+            'xquartz': this.settings.get('xquartz-autostart'),
+            'explicit': flags.explicit
+        })
+    // -- run basic job --------------------------------------------------------
+    const fixed_flags = {"remove-on-exit": (flags['file-access'] === "bind")}
+    const { job, job_data } = this.runSimpleJobAndCopy({ ... flags, ... fixed_flags }, argv)
+    printValidatedOutput(job_data)
+    printValidatedOutput(job)
+  }
+
+}
