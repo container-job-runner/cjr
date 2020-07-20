9ec87e6512013dd8fb7c66cc794f74f6df1fcf63
buvoli <buvoli@uw.edu>
2020-07-20 03:12:12 +0000

    listProject now shows profiles

--- src/lib/commands/project-settings-command.ts
+++ src/lib/commands/project-settings-command.ts
@@ -18,7 +18,9 @@ export abstract class ProjectSettingsCommand extends BasicCommand
   listProject(project_settings: ProjectSettings, project_root: string)
   {
     const raw_settings:Dictionary = project_settings.get(["project-root", "stack", "stacks-dir", "resource", "visible-stacks"])
-    console.log(chalk`\n   {bold Project}: ${project_root}\n`)
+    console.log(chalk`\n   {bold Project}: ${project_root}`)
+    
+    this.printHeader('settings:')
     Object.keys(raw_settings).map((key:string) => {
       const value = raw_settings[key]
       let value_str = value
@@ -29,16 +31,35 @@ export abstract class ProjectSettingsCommand extends BasicCommand
       this.printKeyVal(key, value_str)
     })
 
+    // -- print project profiles -----------------------------------------------
+    const project_profiles = this.getProjectProfiles(project_root)
+    if( project_profiles.length > 0 ) {
+        this.printHeader('profiles:')
+        project_profiles.map( (name: string, index: number) => {
+            this.printVal(name, index + 1)
+        }) 
+    }
+
     // -- print default-profiles -----------------------------------------------
     const default_profiles = project_settings.getDefaultProfiles()
     const default_profiles_names = Object.keys(default_profiles || {});
     if( default_profiles && default_profiles_names.length > 0 ) {
-      console.log(chalk`\n   {underline default-profiles:}`)
+      this.printHeader('default profiles:')
       default_profiles_names.map( (name: string, index: number) => {
         this.printKeyVal(name, default_profiles[name].includes(project_settings.profile_all_stacks_keyword) ? "ALL" : this.arrayToConsoleStr(default_profiles[name], 6), index + 1)
       })
     }
     console.log()
+  }
+
+  private getProjectProfiles(project_root: string) : Array<string>
+  {
+    const project_profile_path = constants.projectSettingsProfilePath(project_root)
+    if(FileTools.existsDir(project_profile_path))
+        return fs.readdirSync(project_profile_path)
+            .filter( ( file_name: string ) => /\.yml$/.test(file_name) )
+            .map( ( file_name: string ) => file_name.replace(/\.yml/, "") )
+    return []
   }
 
   copyProfileToProjectSettings(config_path: string, project_root: string)
@@ -69,6 +90,15 @@ export abstract class ProjectSettingsCommand extends BasicCommand
   printKeyVal(key: string, value: string, index?: number) {
     const header = (index) ? `${index}. ` : ""
     console.log(chalk`   ${header}{italic ${key}}:`, value)
+  }
+
+  printVal(value: string, index?: number) {
+    const header = (index) ? `${index}. ` : ""
+    console.log(chalk`   ${header}${value}`,)
+  }
+
+  printHeader(name: string) {
+    console.log(chalk`\n   {underline ${name}}`)
   }
 
 }
