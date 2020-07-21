e21f59887a527458342f4eb6d084e1f3c537d3f8
buvoli <buvoli@uw.edu>
2020-07-21 01:47:13 +0000

    Removed snapshotting from shell command

--- src/commands/shell.ts
+++ src/commands/shell.ts
@@ -1,9 +1,7 @@
 import { flags } from '@oclif/command'
 import { printValidatedOutput } from '../lib/functions/misc-functions'
-import { initX11, snapshot, promptUserToSnapshot, augmentImagePushParameters } from '../lib/functions/cli-functions'
+import { initX11 } from '../lib/functions/cli-functions'
 import { JobCommand } from '../lib/commands/job-command'
-import { StackConfiguration } from '../lib/config/stacks/abstract/stack-configuration'
-import { ContainerDrivers, OutputOptions } from '../lib/job-managers/abstract/job-manager'
 
 export default class Shell extends JobCommand {
   static description = 'Start an interactive shell for development on localhost.'
@@ -21,8 +19,7 @@ export default class Shell extends JobCommand {
     "no-autoload": flags.boolean({default: false, description: "prevents cli from automatically loading flags using project settings files"}),
     "stacks-dir": flags.string({default: "", description: "override default stack directory"}),
     "working-directory": flags.string({default: process.cwd(), description: 'cli will behave as if it was called from the specified directory'}),
-    "build-mode":  flags.string({default: "reuse-image", description: 'specify how to build stack. Options include "reuse-image", "cached", "no-cache", "cached,pull", and "no-cache,pull"'}),
-    "snapshot": flags.boolean({default: false, description: "changes will written to new snapshot; only affects stacks with snapshot flag."})
+    "build-mode":  flags.string({default: "reuse-image", description: 'specify how to build stack. Options include "reuse-image", "cached", "no-cache", "cached,pull", and "no-cache,pull"'})
   }
   static strict = true;
 
@@ -41,53 +38,14 @@ export default class Shell extends JobCommand {
       "file-access": "bind",
       "label": [],
       "sync": true,
-      "remove-on-exit": false
+      "remove-on-exit": true
     }
     const {job, job_data} = this.runSimpleJob(
       { ... flags, ... shell_flags},
       [this.settings.get("default-container-shell")]
     )
-    if(job.success && job_data.success) {
-      await this.updateSnapshot(
-        job.value.id,
-        job_data.value.stack_configuration,
-        job_data.value.job_manager.container_drivers,
-        job_data.value.job_manager.output_options,
-        flags['snapshot']
-      )
-      job_data.value.job_manager.container_drivers.runner.jobDelete([job.value.id])
-    }
     printValidatedOutput(job_data)
     printValidatedOutput(job)
-  }
-
-  async updateSnapshot(job_id: string, stack_configuration: StackConfiguration<any>, drivers: ContainerDrivers, output_options: OutputOptions, cli_snapshot_flag: boolean)
-  {
-    const vPrint = (s:string, newline: boolean = true) => (output_options.quiet) ? undefined : ((newline) ? console.log(s) : process.stdout.write(s))
-    const snapshot_options = stack_configuration.getSnapshotOptions();
-
-    if(snapshot_options === undefined)
-      return
-
-    if(snapshot_options['mode'] === "off")
-      return
-
-    if(snapshot_options['mode'] === "flag" && !cli_snapshot_flag)
-      return
-
-    if(snapshot_options['mode'] === "prompt" && !(await promptUserToSnapshot(this.settings.get('interactive'))))
-      return
-
-    vPrint("Saving snapshot...", false)
-    const registry_options = {
-      "username": snapshot_options.username || this.settings.get('container-registry-user'),
-      "server": snapshot_options.server || this.settings.get('container-registry'),
-      "token": snapshot_options.token
-    }
-
-    await augmentImagePushParameters(registry_options)
-    snapshot(job_id, stack_configuration, drivers, registry_options)
-    vPrint("done")
   }
 
 }
