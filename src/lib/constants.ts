8e797153383b1b9c7e85cb93d7423dbc1b951212
buvoli <tommasosignup@gmail.com>
2020-12-24 13:17:37 +0000

    Updated cli default value of timeout-theia to 2

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -124,7 +124,7 @@ export const defaultCLISettings = (config_dir:string, data_dir:string, cache_dir
       "vnc-password": "password",
       "on-vnc-start": on_vnc_start_cmd,
       "timeout-jupyter": "10",
-      "timeout-theia": "10",
+      "timeout-theia": "2",
       "xquartz-autostart": false
   }
 }
